bfe681d9a85c29abbb7523f072a0ef3d3e021b8d
Pablo Pignolo <pablo.pignolo@gmail.com>
2024-01-21 04:29:37 -0300

    Update utility-functions.php

--- includes/utility-functions.php
+++ includes/utility-functions.php
@@ -1,2 +1,8 @@
 <?php
+
+	function getValueFromWpAdminSettings($name,$defaultValue){
+		$value = (!empty(get_option($name))) ? strip_tags(get_option($name)) : $defaultValue;
+		return $value;
+	}
+
 ?>
